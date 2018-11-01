c8ef222ea56682e48ea7a69c45a02f212ddb5a7a
Jungmin Kim <vencott@applepie4.com>
2018-11-01 16:16:13 +0900

    7. Intro to Redux

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet } from 'react-native';
 import Timer from './src/components/Timer';
 
-export default class App extends React.Component {
+export default class App extends Component {
   render() {
     return <Timer />;
   }
